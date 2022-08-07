41fb1c9e22bf58cff14ca4b023fa6e80cfb6e600
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-08-07 05:17:59 +0200

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css'
+import Alpine from 'alpinejs'  
+import { Users} from './users'
+
+window.Alpine = Alpine
+Alpine.data('users', Users)
+
+Alpine.start()
---
+++ users.js
@@ -0,0 +1,99 @@
+import axios from 'axios'
+
+export function Users(){
+    return {
+       user : {
+        loginEmail: '',
+        loginPassword: '',
+        name: '',
+        email: '',
+        password: '',
+        contact: ''
+       }, 
+
+       request: {
+        type: '',
+        description: ''
+       },
+       successText:'',
+       isLoggedIn: true,
+       isLoggingIn: false,
+       isRegistering: true,
+       isMakingRequest: false,
+       showFeed: false,
+       isChatting: false,
+
+
+       register(){
+        axios
+            .post(`https://girl-connect.herokuapp.com/connect/register`, {
+                full_name: this.user.name,
+                email: this.user.email,
+                password: this.user.password,
+                contact: this.user.contact
+            })
+            .then(data => {
+                console.log(data)
+                this.isLoggingIn = true
+                this.isRegistering = false
+            })
+            .catch(error => console.log(error))
+       },
+
+       login(){
+        axios
+            .post(`https://girl-connect.herokuapp.com/connect/login`, {
+                email: this.user.loginEmail,
+                password: this.user.loginPassword
+            })
+            .then(data => {
+                console.log(data)
+                this.isLoggedIn = false
+                this.isLoggingIn = false
+                this.isRegistering = false
+                this.showFeed = true
+            })
+            .catch(error => console.log(error))
+       },
+
+       getCurrentUser(){
+        return this.user.loginEmail
+       },
+
+       showReg(){
+        this.isLoggingIn = false
+        this.isRegistering = true
+       },
+
+       showLogin(){
+        this.isLoggingIn = true
+        this.isRegistering = false
+       },
+
+       showReq(){
+        this.isLoggedIn = true
+        this.isMakingRequest = true
+        this.showFeed = false
+       },
+       createRequest(){
+        axios
+            .post(`https://girl-connect.herokuapp.com/connect/request`, {
+                type: this.request.type,
+                description: this.request.description,
+                email: this.getCurrentUser()
+            }).then(data => {
+                console.log(data)
+                this.request.description = ''
+                this.request.type = ''
+                this.showFeed = true
+                this.isLoggedIn = false
+            })
+       },
+
+       getRequests(){
+        axios
+            .get(`https://girl-connect.herokuapp.com/connect/feeds`)
+            .then(data => console.log(data))
+       }
+    }
+}
